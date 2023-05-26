bebc86893ad8f50a4f0232c031f145a7666c4ed1
Kate2111 <noskova2111@mail.ru>
2023-05-26 15:33:47 +0400

    tasks from the textbook

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //1
+  function showNum(number) {
+    alert(number)
+  }
+
+  //2
+  const arr = ['a', 'b', 'c', 'd', 'e'];
+  const res = arr.map(function(item, index) {
+		return <li key={index}>{item}</li>;
+	});
+	
+  //3
+  const users = [
+    {id: 1,name: 'user1', surn: 'surn1', age: 30},
+    {id: 2,name: 'user2', surn: 'surn2', age: 31},
+    {id: 3,name: 'user3', surn: 'surn3', age: 32},
+  ];
+
+  const userShow = users.map(function(elem) {
+    return <li key={elem.id}>Имя: {elem.name}, Фамилия: {elem.surn}, Возраст: {elem.age}</li>
+  })
+
+  //4
+  const usersTable = [
+    {id: 1, name: 'user1', surn: 'surn1', age: 30},
+    {id: 2, name: 'user2', surn: 'surn2', age: 31},
+    {id: 3, name: 'user3', surn: 'surn3', age: 32},
+  ];
+
+  const showTable = usersTable.map(function(elem) {
+    return <tr key={elem.id}>
+              <td>{elem.name}</td>
+              <td>{elem.surn}</td>
+              <td>{elem.age}</td>
+            </tr>
+  
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* task 1 */}
+      <div className="border">
+        <h5>Сделайте так, чтобы по клику на первую кнопку алертом выводилось число 1, по клику на вторую кнопку - число 2, а по клику на третью - число 3.</h5>
+        <button onClick={() => showNum(1)}>act1</button>
+        <button onClick={() => showNum(2)}>act2</button>
+        <button onClick={() => showNum(3)}>act3</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      {/* task 2 */}
+      <div className="border">
+        <h5>С помощью метода map сформируйте следующий код</h5>
+        <ul>
+          {res}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      {/* task 3 */}
+      <div className="border">
+        <h5>Выведите элементы этого массива в виде списка ul.</h5>
+        <ul>
+          {userShow}
+        </ul>
+      </div>
+
+      {/* task 4 */}
+      <div className="border">
+        <h5>Выведите элементы этого массива в виде таблицы table так, чтобы каждое поле объекта попало в свой тег td. Сделайте заголовки колонок вашей таблицы.</h5>
+        <table>
+          <thead>
+            <tr>
+              <td>Имя</td>
+              <td>Фамилия</td>
+              <td>возраст</td>
+            </tr>
+          </thead>
+          <tbody>
+            {showTable}
+          </tbody>
+        </table>
+      </div>
+      
     </>
   )
 }
